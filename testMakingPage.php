2ade8902a0d3862f56a6363904e708b187e3cfd4
bernike <btjahj5697@students.pcci.edu>
2015-03-02 00:38:52 -0600

    3/2/15 bernike

--- testMakingPage.php
+++ testMakingPage.php
@@ -49,12 +49,11 @@
       <div id="header">
          <img src="images/header.png" class="header"/>
          <img src="images/logo.png" class="testLogo"/>
-         <div class="title"></div>
+
          <form action="logout.php" method="post">
             <input type="submit" value="Sign out" class="logout-button">
          </form>
       </div>
-
    <div id="wrap">
          <div id="content">
             <table>
@@ -62,7 +61,6 @@
 
                   <td id="left">
                      <br />
-                     You are login as <span> </span> <br />
                      <div class="informationForm">
                         Class : &nbsp;
                         <select class="inputs">
@@ -71,33 +69,42 @@
                            <option value="CS414">CS 414</option>
                            <option value="BA303">BA 303</option>
                         </select>
-                        Section : &nbsp;&nbsp;<select class="inputs">
+                        &nbsp;
+                        Section : <select class="inputs">
                            <option value="1">1</option>
                            <option value="2">2</option>
                            <option value="3">3</option>
                         </select><br />
-                        Start : &nbsp;&nbsp;<input type="date" name="startDate" class="inputs"> <input type="time" class="inputs" name="startTime"><br />
-                        End  : &nbsp;&nbsp;<input type="date" id="endDate" class="inputs"> <input type="time" class="inputs" name="endTime"><br />
-                        Time limit : &nbsp;<input type="number" name="hours" min="0" max="10" class="inputs" placeholder="1"> &nbsp; hr &nbsp;
-                        <input type="number" name="minute" min="0" max="60" class="inputs" placeholder="45"> &nbsp; min
+                        Start : &nbsp;&nbsp;&nbsp;&nbsp;<input type="date" name="startDate" class="inputs"> <input type="time" class="inputs" name="startTime"><br />
+                        End : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="date" id="endDate" class="inputs"> <input type="time" class="inputs" name="endTime"><br />
+                        Time limit : &nbsp;<input type="number" name="hours" min="0" max="10" class="inputs" placeholder="0"> hr &nbsp;
+                        &nbsp;&nbsp;&nbsp;
+                        <input type="number" name="minute" min="0" max="60" class="inputs" placeholder="50"> min
+                     </div>
+                     <br />
+                     <div id="publish"></div>&nbsp;&nbsp;&nbsp;
+                     <div id="save"></div>&nbsp;&nbsp;&nbsp;
+                     <div id="preview"></div>&nbsp;&nbsp;&nbsp;
+                     <div id="cancel"></div>
+                     <div id="optionName">Publish&nbsp;&nbsp;&nbsp;
+                        Save&nbsp;&nbsp;&nbsp;
+                        Preview&nbsp;&nbsp;
+                        Cancel
                      </div>
                      <form id="buttonArea" >
                         <div id="form_question"> <!-- Question Types (YC) -->
-                           Question Form : - <br>
                            <ul id="sortable1" class="connectedSortable">
-
                            </ul>
                         </div>
                      </form>
                   </td>
-
                   <td id="middle">
                      <div class="scroll">
                         Test Name:  &nbsp;<input type="text" name="testName" class="inputs" placeholder="Test #1">
 
                         <div>
                            <div id="text_instruc_heading">CS 414 Test Instruction</div>
-                           <textarea id="test_inst_text" name="test_instruc_text" rows="4">Enter Test Instructions . . .</textarea>
+                           <textarea id="test_inst_text" name="test_instruc_text" rows="4" placeholder="Enter Test Instructions . . ."></textarea>
                         </div>
 
                         <hr align="left"> 
@@ -111,23 +118,24 @@
                         </div>
                         <div>
                         PLEDGE:<br />
-                        <input id="pledge_text" type="text" name="pledge" class="inputs"
-                           value="This is a pledge. This is a pledge. This is a pledge. This is a pledge. This is a pledge. This is a pledge. "
-                           style="width: 98%; height:100px;"> <!-- width:600px; -->
+                        <textarea id="pledge_text" type="text" name="pledge" class="inputs"
+                                  style="width: 98%; height:100px;">
+                           This test is completely my own work.
+                           I have not had anyone or anything aid me in answering the questions,
+                           including Internet search engines, such as Google, Bing, ect.
+                           I have not received, nor will I give any information regarding this test.
+                           </textarea> <!-- width:600px; -->
                         </div>
                      </div>
                   </td>
-                  </td>
-
                </tr>
             </table>
          </div>
    </div>
 
       <div id="footer">
-         <img src="images/footerblue.png" class="footerblue"/>
          &copy; MegaMonkey Group - Pensacola Christian College 2015
       </div>
-
+   <img src="images/footerblue.png" class="footerblue"/>
    </BODY>
 </HTML>
