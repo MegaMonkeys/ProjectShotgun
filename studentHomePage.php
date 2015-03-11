95752d632e0b4d5a1904195c2f02c493148171a5
YoungChanYoun <youngchanyoun@gmail.com>
2015-03-11 10:30:59 -0500

    YC - DB Connection Error Change

--- studentHomePage.php
+++ studentHomePage.php
@@ -35,7 +35,7 @@
       //Connect to the MySQL Server
       //Warning Disabled
    if (!@mysqli_real_connect($connection,'CSWEB.studentnet.int', 'team2_cs414', 't2CS414', 'cs414_team2')) {
-      if (!@mysqli_real_connect($connection,'localost', 'team2', 'team2', 'cs414_team2')) {
+      if (!@mysqli_real_connect($connection,'localhost', 'team2', 'team2', 'cs414_team2')) {
          die("<br>Connect Error : " . mysqli_connect_error()); }}
       
       
